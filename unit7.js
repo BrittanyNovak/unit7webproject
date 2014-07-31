29c5a814ce9b5906e9f172707abe5c770f4e4524
BrittanyNovak <brittanynicole0106@gmail.com>
2014-07-31 18:39:55 -0500

    unit 7 mad libs project

---
+++ unit7.js
@@ -0,0 +1,12 @@
+function showTextBoxes(){
+	var box1 = $("#one").val();
+	var box2 = $("#two").val();
+	var box3 = $("#three").val();
+	var box4 = $("#four").val();
+	var box5 = $("#five").val();
+	var box6 = $("#six").val();
+	var box7 = $("#seven").val();
+	alert(box1 + box2 + box3 + box4 + box5 + box6 + box7);
+	$(".odd").val("");
+	$(".even").val("");
+}
