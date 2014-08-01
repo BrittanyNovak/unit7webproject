d72522141fbbcdc9ea7d4e954dc5af1c94177da2
BrittanyNovak <brittanynicole0106@gmail.com>
2014-07-31 20:49:28 -0500

    still trying to add alert

--- unit7.js
+++ unit7.js
@@ -1,13 +1,35 @@
-function showTextBoxes(){
-	var box1 = $("#one").val();
-	var box2 = $("#two").val();
-	var box3 = $("#three").val();
-	var box4 = $("#four").val();
-	var box5 = $("#five").val();
-	var box6 = $("#six").val();
-	var box7 = $("#seven").val();
-	alert(box1 + box2 + box3 + box4 + box5 + box6 + box7);
-	$(".odd").val("");
-	$(".even").val("");
+function checkForm() {
+	var t1, t2, t3, t4, t5, t6, t7;
+	t1 = $("#one").val();
+	t2 = $("#two").val();
+	t3 = $("#three").val();
+	t4 = $("#four").val();
+	t5 = $("#five").val();
+	t6 = $("#six").val();
+	t7 = $("#seven").val();
+	
+	alert(t1 + t2 + t3 + t4 + t5 + t6 + t7);
+	
+	var errorMessage ="";
+	if(t1.val == ""){ errorMessage += " Missing:Input 1";)
+	t1.addClass("error")}
+	else ( v1.removeClass("error");
+	}
+	if(t2.val == ""){ errorMessage += " Missing:Input 2";)
+	if(t3.val == ""){ errorMessage += " Missing:Input 3";)
+	if(t4.val == ""){ errorMessage += " Missing:Input 4";)
+	if(t5.val == ""){ errorMessage += " Missing:Input 5";)
+	if(t6.val == ""){ errorMessage += " Missing:Input 6";)
+	if(t7.val == ""){ errorMessage += " Missing:Input 7";)
+	
+	if(errorMessage != "") {
+		alert(errorMessage);
+		} else {
+			alert( "The itsy bitsy " + t1 + " climbed up the " + t2 + " itsy bitsy realized he " + t3 " he climbed up on the " + t4 + " before " + t5 " the itsy bitsy " + t6 + " burned his little " + t7)
+		}
+		
+	
+	
+	
 }
 
